bff1306437d57d6902658a00381e6daade33a707
Evgeniy <smoke@weed.pm>
2026-04-14 09:16:38 +0300

    1

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ justify-content:center;height:100vh;font-family:system-ui;font-size:4vw">
 <div style="text-align:center">
 <div style="width:60px;height:60px;border:4px solid #333;border-top:4px solid #fff;
 border-radius:50%;animation:spin 1s linear infinite;margin:0 auto 30px"></div>
-Загрузка123
+Загрузка
 </div>
 <style>@keyframes spin{to{transform:rotate(360deg)}}</style>
 </body>
