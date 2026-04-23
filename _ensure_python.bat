c7f3dba8e489811e1d7fdca965a428cb645c346a
Evgeniy <smoke@weed.pm>
2026-04-23 16:20:10 +0300

    update v2

--- _ensure_python.bat
+++ _ensure_python.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 if exist "python\python.exe" goto :install_deps
 
 echo Downloading Python embeddable...
-set PYVER=3.14.0
+set PYVER=3.12.9
 powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/%PYVER%/python-%PYVER%-embed-amd64.zip' -OutFile python-embed.zip; Expand-Archive python-embed.zip -DestinationPath python; Remove-Item python-embed.zip"
 echo Enabling site-packages...
 powershell -Command "$f = Get-ChildItem python -Filter 'python*._pth'; $c = Get-Content $f.FullName; $c = $c -replace '#import site','import site'; $c += \"`n..\"; Set-Content $f.FullName $c"
